950f5979d8c91a35c7d28b9dc5cdd102d44c96d2
Sylvain Bekaert <sylvain.bekaert@gmail.com>
2017-04-27 10:59:26 +0200

    Front: add Player component

--- src/ui/components/app.js
+++ src/ui/components/app.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Player from './player.js';
 
 export default class App extends React.Component {
   render() {
-    return <h1 className="title">Hello World</h1>
+    return <div>
+      <Player />
+    </div>;
   }
 }
---
+++ src/ui/components/player.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Player extends React.Component {
+  render() {
+    return <video src='assets/videos/StudioCanalTheThirdMan.mp4' autoPlay="true" />
+  }
+}
