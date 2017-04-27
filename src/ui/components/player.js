53b419c0d66ce7ac4becce6b4e2a2ac7b47d515d
Sylvain Bekaert <sylvain.bekaert@gmail.com>
2017-04-27 12:18:29 +0200

    Front: add welcome screen

--- src/ui/components/player.js
+++ src/ui/components/player.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default class Player extends React.Component {
   render() {
-    return <video src={this.props.src} autoPlay="true" />
+    return <video
+      src={this.props.src}
+      ref={(video) => { this.video = video; }}
+      autoPlay="true" />
+  }
+
+  componentDidMount() {
+    this.video.onplay = this.props.onVideoLoaded;
   }
 }
